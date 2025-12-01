48608ab6946f237edb0257d503f1ca57ee13cdf3
Joseph Raymond <josephranjalahy@gmail.com>
2025-12-01 13:16:53 +0100

    Make humanChoice (if + switch) and computerChoice
    Real example of switch statement

--- script.js
+++ script.js
@@ -1,103 +1,35 @@
-// Exercise : Copy and sort array
-// Two phases : create a copy of the array, then sort that copy
+console.log("Hello World!");
 
-let fifthExerciseArray = ["HTML", "JavaScript", "CSS"];
-let newArray = fifthExerciseArray.slice();
-newArray.sort();
+function getComputerChoice() {
+  let result = Math.floor((Math.random()) * 3);
 
-// Exercise : Shuffle an array
-// Create a function that randomly reorders elements of an array
-
-let sixthExerciseArray = [1, 2, 3];
-
-function randomize () {
-  for (let i = 0; i < sixthExerciseArray.length; i++);
-  return Math.random()
+  if (result == 0) return "rock";
+  if (result == 1) return "paper";
+  else return "scissors";
 }
 
-function shyffle () {
-  return randomize(sixthExerciseArray).sort((a, b) => a - b);
-}
-// ChatGPT exercise
+function getHumanChoice() {
+  let answer = prompt("Enter a value").toLowerCase();
 
-const allNumbers = [1, 2, 2, 3, 3, 4, 5, 6, 6, 7, 8, 9];
-
-function getEvenNumbers(arr) {
-  let resultant = [];
-
-  for (let number of allNumbers) {
-  if (number % 2 != 0)
-    resultant.push(number)
-  }
-
-return resultant;
+  if (answer == "rock") return "rock";
+  if (answer == "paper") return "paper";
+  if (answer == "scissors") return "scissors";
+  else return "Not a valid answer";
 }
 
-console.log(getEvenNumbers(allNumbers));
+// Same, but in a switch statement
 
-// Another ChatGPT exercise
+// function getHumanChoice() {
+  // let answer = prompt("Enter a value");
 
-const givenNumbers = [1, 2, 3, 3, 5, 6, 7];
-
-function tripleOddNumbers(brr) {
-  let tripleResult = [];
-
-  for (let given of givenNumbers) {
-    if (given % 2 != 0 && !tripleResult.includes(given))
-      given*3 && tripleResult.push(given)
-
-    return tripleResult;
-}
-}
-
-tripleOddNumbers(givenNumbers);
-
-// Another ChatGPT exercise
-
-const listofStrings = ["Magenta", "Ci", "Bleu", "Re", "Dragon"];
-
-function filterLongStrings(crr) {
-  let longStrings = []
-
-  for (let element of crr) {
-    if (typeof element == 'string') {
-    if (element.length > 3) {
-      longStrings.push(element);
-      }
-    }
-  }
-  return longStrings;
-}
-
-// Exercice ChatGPT : les variables intermédiaires (1)
-
-function doublePositiveNumbers(arroyo) {
-  let arroyoResult = []
-
-  for (let carlos of arroyo) {
-    if (carlos > 0 && !arroyoResult.includes(carlos*2)) {
-      let doubleCarlos = carlos*2;
-    arroyoResult.push(doubleCarlos)
-  }
-}
-  return arroyoResult;
-}
-
-// Exercice ChatGPT : les variables intermédiaires (2)
-
-function cleanAndFormatStrings(wade) {
-  let lebron = []
-
-  for (let dwyane of wade) {
-    if (typeof dwyane == 'string') {
-      let bosh = dwyane.trim().toLowerCase().replace(" ", "-");
-      if (!lebron.includes(bosh)) {
-        lebron.push(bosh)
-      }
-    }
-  }
-  return lebron;
-}
-
-const someItems = [" Hello World ", "hello  world", " test ", 35, "TEST"]
-console.log(cleanAndFormatStrings(someItems));
+  // switch (answer) {
+    // case "rock":
+      // return "rock";
+    // case "paper":
+      // return "paper";
+    // case "scissors":
+      // return "scissors";
+    // default:
+      // return "Not a valid answer";
+  // }
+// }
